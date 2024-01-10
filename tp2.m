e9daca3d07fbca7d8fbdac1d134fac2309c441a3
amzarzaidi <147496470+amzarzaidi@users.noreply.github.com>
2024-01-10 15:17:13 +0100

    Update tp2.m

--- tp2.m
+++ tp2.m
@@ -8,6 +8,7 @@ for i = 1:length(M)
     BER(:,i) = SER / log2(M(i));
 end
 
+BER256pratique = [0.3942, 0.3872, 0.3796, 0.3175, 0.3625, 0.3527, 0.3425, 0.3316, 0.3201, 0.3077, 0.2946, 0.2807, 0.2661, 0.2507, 0.2345, 0.2173, 0.1987, 0.1787, 0.1574, 0.1354, 0.1129, 0.0908, 0.0699, 0.0509, 0.03477];
 BER4 = BER(:,1);
 BER16 = BER(:,2);
 BER64 = BER(:,3);
@@ -22,8 +23,10 @@ semilogy(RSB,BER64);
 hold on;
 semilogy(RSB,BER256);
 hold on;
+semilogy(RSB,BER256pratique);
 
 grid on;
 axis([0 24 1e-10 1]);
-xlabel('RSB (dB)');
+xlabel('RSB ');
 ylabel('BER');
+legend('4','16','64','256-theorique','256-pratique');
